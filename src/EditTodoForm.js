ba314b5dc3758702dfff35362230063c3a5bc418
kenrazalan <kennethrazalan29@gmail.com>
2020-07-20 23:34:39 +0800

    add-context

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,8 +1,10 @@
-import React, { useDebugValue } from 'react';
+import React, { useContext } from 'react';
 import TextField from '@material-ui/core/TextField';
 import useInputState from './Hooks/useInputState';
+import {TodosContext} from './contexts/todos-context';
 
-function EditTodoForm({editTodo,id,task,toggle}){
+function EditTodoForm({id,task,toggle}){
+  const {editTodo} = useContext(TodosContext);
     const [value,handleChange,reset]= useInputState(task);
     return (
         <form onSubmit={(e)=>{
