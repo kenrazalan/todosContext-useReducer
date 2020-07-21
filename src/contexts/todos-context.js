ae5586949862388cf85d1e7c7be4584fd6cf26d5
kenrazalan <kennethrazalan29@gmail.com>
2020-07-21 19:55:08 +0800

    add-custom reducer-hook

--- src/contexts/todos-context.js
+++ src/contexts/todos-context.js
@@ -1,16 +1,18 @@
-import React,{createContext,useReducer} from 'react';
+import React,{createContext} from 'react';
+import useLocalStorageReducer from '../reducer/useLocalStorageReducer';
 import todoReducer from '../reducer/todo-reducer';
 
 const defaultTodos =[
-    {id:1, task:"ads", completed: false},
-    {id:2, task:"sdf g", completed: false}
+    {id:1, task:"Code", completed: false},
+    {id:2, task:"Eat", completed: false},
+    {id:3,task:"Sleep", completed:true}
 ]
 
 export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props){
-    const [todo,dispatch]= useReducer(todoReducer,defaultTodos);
+    const [todo,dispatch]= useLocalStorageReducer("todo",defaultTodos,todoReducer);
     return(
         <TodosContext.Provider value={todo}>
             <DispatchContext.Provider value={dispatch}>
